6722e6bb269587df5b1934c1b3076430a9799262
Andrea Ghidara <Ghidara.andrea.99@gmail.com>
2023-05-08 15:53:42 +0200

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,12 +10,18 @@ createApp({
             surname: "",
             narutoLink: "https://i0.wp.com/www.meganerd.it/wp-content/uploads/2023/03/Naruto-quattro-nuovi-episodi.jpg?resize=1024%2C576&ssl=1",
             linkCustom: "",
+            color:"",
         }
     },
     methods: {            
         greting: function() {
             return 'ciao';
         },
+        changeBg() {
+            let oggetto = document.querySelector(".color");
+            oggetto.style.backgroundColor = this.color;
+            console.log(this.color);
+        }
         
     }
 }).mount('#app')
